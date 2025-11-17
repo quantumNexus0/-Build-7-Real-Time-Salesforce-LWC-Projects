ce6f1e6331d080d98a7d8917e72dab547be4a882
vipulyadav503@gmail.com <vipulyadav503@gmail.com>
2025-11-17 08:54:24 +0530

    Updated portfolioUserDetailsAndStatsWrapper component

--- force-app/main/default/lwc/portfolioUserDetailsAndStatsWrapper/portfolioUserDetailsAndStatsWrapper.js
+++ force-app/main/default/lwc/portfolioUserDetailsAndStatsWrapper/portfolioUserDetailsAndStatsWrapper.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class PortfolioUserDetailsAndStatsWrapper extends LightningElement {}
+export default class PortfolioUserDetailsAndStatsWrapper extends LightningElement {
+    recordID="a01gL00000VIj4sQAD";
+    objectApiName="Portfolio__c";
+}
